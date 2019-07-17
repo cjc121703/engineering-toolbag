7659097f3d68c7df08b878f4f02d9067d5a27922
Christine Carroll <christine.carroll@thomsonreuters.com>
2019-07-17 13:13:58 -0500

    Making a new component to draw on a picture

---
+++ src/components/DrawLinesOnAPicture.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <v-stage :config="configKonva">
+            <v-layer>
+                <v-image :config="{
+                        image: image,
+                        width:imageWidth,
+                        height:imageHeight
+                    }"/>
+                <v-line :config="configLine"></v-line>
+                <v-rect :config="configPoint"></v-rect>
+            </v-layer>
+        </v-stage>
+    </div>
+</template>
+<script>
+export default {
+    name: 'DrawLinesOnAPicture',
+    props: [
+        'imageSrc',
+        'imageHeight',
+        'imageWidth',
+        'configPoint'
+    ],
+    data () {
+        return{
+            image: null,
+            configKonva: null,
+            configLine: {
+                x: 0    ,
+                y: 300,
+                points: [0, 0, 300, 40, 25, 25],
+                stroke: 'black'
+            }
+        }
+    },
+    created() {
+        const image = new window.Image();
+        image.src = this.imageSrc;
+        this.configKonva = {
+            width: this.imageHeight,
+            height: this.imageWidth
+        };
+        image.onload = () => {
+            // set image only when it is loaded
+            this.image = image;
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
     <v-container grid-list-xs fluid>
-        <v-flex md6 offset-md3>
+        <v-flex lg4 md6 offset-lg4 offset-md3>
             <v-card>
                 <v-layout row wrap>
                     <v-flex xs12 title text-xs-center>
                         Welcome to the beta site for engineering tools.
                     </v-flex>
-                    <v-flex xs12 text-xs-center>
+                    <v-flex class="subheading" xs12 text-xs-center>
                         Sign in!
                     </v-flex>
-                    <v-flex xs12 offset-md1>
+                    <v-flex xs12 offset-md1 >
                         <v-layout row wrap>
                             <v-flex xs12 md2>
-                                <label for="userIdSignIn">User Name:</label>
+                                <label class="body-1 font-weight-bold" for="userIdSignIn">User Name:</label>
                             </v-flex>
                             <v-flex xs12 md9>
                                 <input id="userIdSignIn" placeholder="User Name">
@@ -22,7 +22,7 @@
                     <v-flex xs12 offset-md1>
                         <v-layout row wrap>
                             <v-flex xs12 md2>
-                                <label for="passwordSignIn">Password:</label>
+                                <label class="body-1 font-weight-bold" for="passwordSignIn">Password:</label>
                             </v-flex>
                             <v-flex xs12 md9>
                                 <input type="password" placeholder="Password">
@@ -34,20 +34,90 @@
                     </v-flex>
                 </v-layout>
             </v-card>
+            <v-card>
+                <v-layout row wrap>
+                    <v-flex xs3>
+                        <div class="pointInputs">
+                            <label for="startX">Starting X Point</label>                
+                            <input id="startX" v-model="startX" type="number" min="0" >
+                        </div>
+                    </v-flex>
+                    <v-flex xs1>
+                    </v-flex> 
+                    <v-flex xs3>
+                        <div class="pointInputs">                        
+                            <label for="startY">Starting Y Point</label>                
+                            <input id="startY" v-model="startY" type="number" min="0">
+                        </div>
+                    </v-flex>
+                    <v-flex xs1>
+                    </v-flex>
+                    <v-flex xs3>
+                        <v-btn v-on:click="submitStartPosition()">Submit</v-btn>
+                    </v-flex>
+                    <v-flex xs3>
+                        <v-btn v-if="startPointSumbitted" v-on:click="clearStartPosition()">X</v-btn>                        
+                    </v-flex>
+                </v-layout>
+                <DrawLinesOnAPicture 
+                    :imageSrc='field' 
+                    :imageHeight="fieldHeight" 
+                    :imageWidth="fieldWidth"
+                    :configPoint="configPoint"
+                >
+                </DrawLinesOnAPicture>
+            </v-card>
         </v-flex>
     </v-container>
 </template>
 <script>
+import DrawLinesOnAPicture from "@/components/DrawLinesOnAPicture"
+
   export default {
+    components: {
+      DrawLinesOnAPicture
+    },
     data () {
       return {
-        dialog: false
+          field: require('@/assets/field.jpg'),
+          fieldHeight: 500,
+          fieldWidth:500,
+          startPointSumbitted: false,
+          startX:0,
+          startY:0,
+          configPoint:{
+                x: this.startX,
+                y: this.startY,
+                width: 10,
+                height: 10,
+                fill: "red"
+          }
+
       }
+    },
+    methods:{
+        submitStartPosition : function() {
+          this.configPoint = {
+                x: parseInt(this.startX),
+                y: parseInt(this.startY),
+                width: 10,
+                height: 10,
+                fill: "red"
+          };
+          this.startPointSumbitted = true;
+        }
+    },
+    created() {
     }
   }
 </script>
 <style>
-    .myFont{
-        font-family: "Karla"
+    input{
+        border-bottom:1px solid black;
+        width: 100%;
+        font-size: 16px;
+    }
+    .pointInputs{
+        padding-left: 10px
     }
 </style>
